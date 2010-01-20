9d9ee0807e09076d6151a3ba33e736db8fd7966c
Nick Shepherd <me@nickshepherd.me>
2010-01-20 17:51:34 +0000

    Added Element Parameter to $.addThis(user,elem)

--- jquery.addthis.js
+++ jquery.addthis.js
@@ -6,19 +6,23 @@
  *  $.addthis();
  *   -or-
  *  $.addthis('username');
+ *   -or-
+ *  $.addthis('username','#element');
  *  where username is your AddThis username. Useful for tracking statistics
 */
 (function($){
-	$.addthis = function(code){
+	$.addthis = function(code, elem){
 
 		function init(){
 			try{
 				// determine whether to include the normal or SSL version
 				var addthisurl = (location.href.indexOf('https') == 0 ? 'https://' : 'http://') + 's7.addthis.com/js/250/addthis_widget.js?pub=' + code;
 
+				if(!elem) elem = 'a.addthis';
+
 				// include the script
 				$.getScript(addthisurl, function(){
-					$('a.addthis').append('<img src="http://s7.addthis.com/static/btn/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/>').attr('href', 'http://www.addthis.com/bookmark.php?v=250').mouseover(
+					$(elem).append('<img src="http://s7.addthis.com/static/btn/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/>').attr('href', 'http://www.addthis.com/bookmark.php?v=250').mouseover(
 						function(){
 							return addthis_open(this, '', '[URL]', '[TITLE]');
 						}).mouseout(
